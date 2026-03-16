e733b7afe209fc8db724f051783577bb128e9b33
josejesus3 <gutierresrayas88@gmail.com>
2026-03-15 23:24:47 -0600

    product-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
+import { ProductListComponent } from "./features/pages/product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavBarComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/features/pages/product-list/product-list.component.ts
+++ src/app/features/pages/product-list/product-list.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-
+  productos: Product[] = [
+    {
+      id: 1,
+      title: 'Fjallraven - Foldsack No. 1 Backpack',
+      price: 109.95,
+      description:
+        'Tu mochila perfecta para el uso diario y caminatas en el bosque.',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 2,
+      title: 'Mens Casual Premium Slim Fit T-Shirts',
+      price: 22.3,
+      description: 'Estilo ajustado, manga corta contrastada, tres botones.',
+      category: "men's clothing",
+      image:
+        'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 3,
+      title: 'Mens Cotton Jacket',
+      price: 55.99,
+      description: 'Gran chaqueta de abrigo para primavera/otoño/invierno.',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 4,
+      title: 'Mens Casual Slim Fit',
+      price: 15.99,
+      description: 'La moda es un lenguaje que se crea a sí mismo en la ropa.',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 5,
+      title: "John Hardy Women's Legends Naga Bracelet",
+      price: 695,
+      description: 'Inspirado por la mítica serpiente de agua de Bali.',
+      category: 'jewelery',
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 6,
+      title: 'Solid Gold Petite Micropave',
+      price: 168,
+      description:
+        'Satisfacción garantizada. Regresa o cambia cualquier orden.',
+      category: 'jewelery',
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+    {
+      id: 7,
+      title: 'White Gold Plated Princess',
+      price: 9.99,
+      description: 'Anillo de compromiso clásico creado para ella.',
+      category: 'jewelery',
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png',
+    },
+  ];
 }
