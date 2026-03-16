0b0f0a4d047b7c52a9d040b05574c80f0b72ffa2
josejesus3 <gutierresrayas88@gmail.com>
2026-03-16 01:37:54 -0600

    Navbar-Burgger-ProductDetails

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductDetailComponent } from "../components/product-detail/product-detail.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [ProductDetailComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
+  categories: string[] = ['electronics', 'jewelery', "men's clothing", "women's clothing"];
 }
